5a0cc725f901ffdc5ada4258cf23c27c77ce76ed
Invincible-Shady <zk1263307973@outlook.com>
2017-10-02 17:56:26 +0800

    日期的计算

---
+++ 实验四/日期天数计算/src/CDate.java
@@ -0,0 +1,19 @@
+
+public class CDate {
+	private int day,month,year;
+	public CDate(){
+		
+	}
+	public CDate(int d,int m,int y) {
+		
+	}
+	public int daysInMonth(int m) {
+		
+	}
+	public int daysInMonth(int m,int y) {
+		
+	}
+	public int howManyDays() {
+		
+	}
+}
